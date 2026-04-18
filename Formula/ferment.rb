7465dd12537d24b7a7227b6095bf0360c8116e18
Nick Schneble <nschneble@users.noreply.github.com>
2026-04-18 13:58:57 -0400

    update Homebrew formula for v1.0.1

--- Formula/ferment.rb
+++ Formula/ferment.rb
@@ -1,8 +1,8 @@
 class Ferment < Formula
-  desc "One-click Homebrew daily refresh with a terminal UI"
+  desc "A daily script to keep Homebrew fresh!"
   homepage "https://github.com/nschneble/homebrew-ferment"
-  url "https://github.com/nschneble/homebrew-ferment/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "bef140a1a96994029153dca8c00b1750b9a5a764fb9db2dc68d7bb40e8a29e8a"
+  url "https://github.com/nschneble/homebrew-ferment/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "50be8acb1dd4e76e5077006e8351dbb9e9dc18ab5670c39bd140091cfa7681ef"
   license "MIT"
 
   head "https://github.com/nschneble/homebrew-ferment.git", branch: "main"
